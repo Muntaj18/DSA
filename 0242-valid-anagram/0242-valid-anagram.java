360a3ad81a6989996409b6abe418c1aadc3e0eb4
Shaik Muntaj Begum <144787910+Muntaj18@users.noreply.github.com>
2026-07-02 11:16:52 +0530

    Time: 4 ms (82.22%), Space: 43.9 MB (97.71%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,16 +1,39 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-
-        if(s.length() != t.length())
+        if(s.length() !=t.length())
         {
             return false;
         }
-        String[] sarr=s.split("");
-        String[] tarr=t.split("");
-        Arrays.sort(sarr);
-        Arrays.sort(tarr);
 
-        return Arrays.equals(sarr,tarr);
+        int[] count=new int[26];
+        for(char c:s.toCharArray())
+        {
+            count[c-'a']++;
+        }
+
+        
+        for(char c:t.toCharArray())
+        {
+            count[c-'a']--;
+        }
+
+        for(int i:count)
+
+        {
+            if(i!=0)
+            {
+                return false;
+            }
+        }
+        return  true;
+
+
+
+
+
+
+
+       
         
     }
 }
